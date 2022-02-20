ebb02e249c6414340bfbdeecd8cfa69a23f534f5
K UDAY KUMAR <72476990+UDAY368@users.noreply.github.com>
2022-02-20 07:28:35 +0530

    Completed build the slider with react bootstrap package

--- src/App.js
+++ src/App.js
@@ -3,12 +3,15 @@ import './App.css';
 import Navbar from './components/Navbar';
 import PreNavbar from './components/PreNavbar';
 import {BrowserRouter as Router} from "react-router-dom";
+import Slider from './components/Slider';
+import data from './data/data.json';
 
 function App() {
   return (
     <Router>
       <PreNavbar />
       <Navbar />
+      <Slider start={data.banner.start} />
     </Router>
   );
 }
